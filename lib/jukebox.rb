dfd0df0b9b4e3d9564f3f74cf6e85a40fe7231c9
Liz Love <love.elizabeth1989@gmail.com>
2020-07-21 13:49:48 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -68,4 +68,28 @@ def prompt()
 end
 
   
-d
+def run(my_songs)
+  help()
+  input = prompt()
+  
+  while input != "exit"
+    if input == "list"
+      list(my_songs)
+      input = prompt()
+    elsif input == "play"
+      play(my_songs)
+      input = prompt()
+    elsif input == "help"
+      help()
+      input = prompt()
+    else
+      puts "Invalid command"
+      help()
+      input = prompt()
+    end
+  end
+  
+  exit_jukebox()
+end
+
+run(songs)
