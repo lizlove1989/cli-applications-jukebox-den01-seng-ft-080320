001f7ca1100491cbed8aeb4df193500a1a3b6da0
Liz Love <love.elizabeth1989@gmail.com>
2020-07-21 13:50:09 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -72,26 +72,24 @@ def run(my_songs)
   help()
   input = prompt()
   
-  while input
-    if input != "exit"
-      if input == "list"
-        list(my_songs)
-        input = prompt()
-      elsif input == "play"
-        play(my_songs)
-        input = prompt()
-      elsif input == "help"
-        help()
-        input = prompt()
-      else
-        puts "Invalid command"
-        help()
-        input = prompt()
-      end
-    else 
-      prompt()
-      input == "exit"
-      exit_jukebox()
+  while input != "exit"
+    if input == "list"
+      list(my_songs)
+      input = prompt()
+    elsif input == "play"
+      play(my_songs)
+      input = prompt()
+    elsif input == "help"
+      help()
+      input = prompt()
+    else
+      puts "Invalid command"
+      help()
+      input = prompt()
     end
   end
+  
+  exit_jukebox()
 end
+
+
